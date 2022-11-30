2aef14c67ec20de46896b14de89ea2f02abdd009
Rogerio <rogerioalves.ee@gmail.com>
2022-11-30 12:26:00 -0300

    remove log

--- src/utils/log.py
+++ src/utils/log.py
@@ -1,26 +1,19 @@
 import logging
 import logging.handlers
-from os.path import join
 
 from src.utils.singleton import Singleton
 
 
 class Log(metaclass=Singleton):
-    FILE = "visualizador.log"
     LOGGER = None
 
     @classmethod
     def config_logging(cls, diretorio: str):
         root = logging.getLogger()
-        h = logging.handlers.RotatingFileHandler(
-            join(diretorio, cls.FILE), "a", 10000, 0, "utf-8"
-        )
         f = logging.Formatter("%(asctime)s %(levelname)s: %(message)s")
-        h.setFormatter(f)
         # Logger para STDOUT
         std_h = logging.StreamHandler()
         std_h.setFormatter(f)
-        root.addHandler(h)
         root.addHandler(std_h)
         root.setLevel(logging.INFO)
         cls.LOGGER = root
